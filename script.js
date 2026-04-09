1cb24dd40bc39661485faeb9f15504af8f79e91c
nedalalshami <124066052+nedalalshami@users.noreply.github.com>
2026-04-09 10:14:10 +0300

    تصحيح أسماء الصور إلى أحرف صغيرة

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ const productsData = [
         id: 13,
         name: "  Cupcakes,كب كيك  (كنافة)  ",
         price: 1 ,
-        image: "img/capKekKnafe.jpg",
+        image: "img/capkekknafe.jpg",
         category: "candy3",
         description: "   كب كيك بحشوات متعددة , سعر القطعة 1 يورو , مع عرض 6 قطع ب 5 يورو ,Cupcakes aus kunafa mit verschiedenen Füllungen      "
     },
@@ -123,7 +123,7 @@ const productsData = [
         id: 16,
         name: "  حلاوة الجبن مع الفستق الحلبي",
         price: 15 ,
-        image: "img/hlwetjbnFstk.jpg",
+        image: "img/hlwetjbnfstk.jpg",
         category: "candy3",
         description:   "حلاوة الجبن مع الفستق الحلبي وماء الورد . سعر الكيلو 15 يورو , kase-Halawa -wenn Pistazien , kase und Rosenwasser zusammenkommen"
     }
